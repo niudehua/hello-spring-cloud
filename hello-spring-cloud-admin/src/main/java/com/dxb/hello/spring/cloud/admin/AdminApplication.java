0debf1fad6b2ae4643ba87ebbe2c25c2b03e517e
dehua niu <657563945dxb@gmail.com>
2019-04-19 21:49:10 +0800

    测试sourcetree

--- hello-spring-cloud-admin/src/main/java/com/dxb/hello/spring/cloud/admin/AdminApplication.java
+++ hello-spring-cloud-admin/src/main/java/com/dxb/hello/spring/cloud/admin/AdminApplication.java
@@ -4,7 +4,7 @@ import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-// 测试
+
 
 /**
  * @author:deng
